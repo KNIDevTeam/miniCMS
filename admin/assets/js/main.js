fc9e93366deeabe2cb500593f6a2220c20c31313
Dawid Alimowski <dawid.alimowski@gmail.com>
2019-12-22 21:03:00 +0100

    Comments

--- admin/assets/js/main.js
+++ admin/assets/js/main.js
@@ -1,24 +1,28 @@
-$('.expand').each(function() {
+/* Add buttons to expandable menus */
+
+$(".expand").each(function() {
     $(this).append(`<div class="expand-button expand-button-inactive ${this.id}"><</div>`);
 });
 
-$('.expand').click(function() {
+/* Expand menu on click */
+
+$(".expand").click(function() {
     let id = this.id;
-    $('.submenu').each(function() {
+    $(".submenu").each(function() {
         if($(this).hasClass(id)) $(this).slideToggle();
     });
-    $('.expand-button').each(function() {
+    $(".expand-button").each(function() {
         if($(this).hasClass(id)) {
-            if($(this).hasClass('expand-button-inactive')) {
-                $(this).html('⌄');
-                $(this).css('top', '-0.1em')
-                $(this).removeClass('expand-button-inactive');
-                $(this).addClass('expand-button-active');
+            if($(this).hasClass("expand-button-inactive")) {
+                $(this).html("⌄");
+                $(this).css("top", "-0.1em")
+                $(this).removeClass("expand-button-inactive");
+                $(this).addClass("expand-button-active");
             } else {
-                $(this).html('<');
-                $(this).css('top', '0.3em')
-                $(this).removeClass('expand-button-active');
-                $(this).addClass('expand-button-inactive');
+                $(this).html("<");
+                $(this).css("top", "0.3em")
+                $(this).removeClass("expand-button-active");
+                $(this).addClass("expand-button-inactive");
             }
         }
     });

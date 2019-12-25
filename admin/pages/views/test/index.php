35c1313b4fd38b7e41981cc032ae93a0e08dcf70
RazorMeister <battnik90@gmail.com>
2019-12-25 14:22:19 +0100

    Update route

--- admin/pages/views/test/index.php
+++ admin/pages/views/test/index.php
@@ -1,5 +1,5 @@
 <main>
-    <form method="post" action="<?php echo $this->router->route('testPost') ?>">
+    <form method="post" action="<?php echo route('testPost') ?>">
         <?php crsf() ?>
         <label>Wpisz cos</label>
         <input type="text" name="cebula">

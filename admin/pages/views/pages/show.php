c016fda4041ebae0831a8aae063e7dcded6ca21b
Wojciech Drochomirecki <witek.droch@gmail.com>
2019-12-27 15:25:16 +0100

    first changes in CRUD system, added list of pages and started adding page mechanism

---
+++ admin/pages/views/pages/show.php
@@ -0,0 +1,8 @@
+<main id="main">
+    <div class="logo">
+        <span class="title gradient">About miniCMS</span>
+        Nie zna się zasad gry, a jednak się gra
+        <div class="bottom-border"></div>
+    </div>
+
+</main>

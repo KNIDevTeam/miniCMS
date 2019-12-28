41bb2f6af106b531b4151fe6ddfafb6a469c2cc5
Dawid Alimowski <dawid.alimowski@gmail.com>
2019-12-28 14:54:36 +0100

    Rollback changes

--- admin/pages/views/home.php
+++ admin/pages/views/home.php
@@ -1,6 +1,4 @@
 <main id="main">
-    <div id="blur"></div>
-
     <div class="preface">
         <div class="page-name">mini.pw.edu.pl/~loremipsum</div>
         <div class="page-description">Przegląd strony</div>
@@ -19,43 +17,50 @@
             <span class="tile-bigtitle gradient">1023</span>
             <span class="tile-description">Odwiedzeń w tym tygodniu</span>
         </div>
-        <a class="tile">
-            <span class="tile-title"><i class="fas fa-cog"></i>Ustawienia</span>
+        <div class="tile">
+            <a href="#" class="tile-title nohover"><i class="fas fa-cog"></i>Ustawienia</a>
             <span class="tile-description">Skoro nie wiesz, kim jesteś, przynajmniej miej pewność, kim nie jesteś.</span>
-        </a>
-        <a id="form-popup-show" class="tile">
-            <span class="tile-title"><i class="fas fa-plus"></i>Nowa podstrona</span>
-        </a>
-        <a class="tile">
-            <span class="tile-title"><i class="fas fa-plug"></i>Wtyczki</span>
+        </div>
+        <div class="tile">
+            <a id = "#myLink" href="#" onclick="openAddPageForm()" class="tile-title"><i class="fas fa-plus"></i>Nowa podstrona</a>
+            <span class="tile-description">Nie da się nawet opowiedzieć, dlaczego pewnych rzeczy nie da się opowiedzieć.</span>
+        </div>
+        <div class="tile">
+            <a href="#" class="tile-title"><i class="fas fa-plug"></i>Wtyczki</a>
             <span class="tile-description">Prawda i fałsz nie istnieją na zewnątrz naszych głów.</span>
-        </a>
-        <a class="tile">
-            <span class="tile-title"><i class="fas fa-palette"></i>Paleta</span>
+        </div>
+        <div class="tile">
+            <a href="#" class="tile-title"><i class="fas fa-palette"></i>Paleta</a>
             <span class="tile-description">Ci, co upadli na dno, wiedzą, na czym stoją.</span>
-        </a>
-        <a class="tile">
-            <span class="tile-title"><i class="fas fa-adjust"></i>Hmm</span>
+        </div>
+        <div class="tile">
+            <a href="#" class="tile-title"><i class="fas fa-adjust"></i>Hmm</a>
             <span class="tile-description">Pieniądze, drogi Sasza, są po to, żeby nie musieć zarabiać pieniędzy.</span>
-        </a>
+        </div>
     </div>
 
     <div class="form-popup" id="addPageForm">
         <form action="<?php echo route("addPage"); ?>" class="form-container">
             <h2>Kreator tworzenia nowej podstrony</h2>
-            <label for="pageName">Nazwa strony</label>
+            <br>
+            <label for="pageName"><b>Nazwa strony</b></label>
+            <br>
             <input type="text" placeholder="Podaj nazwę strony" name="pageName" required>
-            <label for="pageParent">Strona nadrzędna</label>
+            <br>
+            <label for="pageParent"><b>Strona nadrzędna</b></label>
+            <br>
             <input type="text" placeholder="Podaj stronę nadrzędną" name="pageParent">
-            <label for="pageTemplate">Szablon</label>
+            <br>
+            <label for="pageTemplate"><b>Szablon</b></label>
             <select name="pageTemplate">
                 <option value="default">Domyślny</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
             </select>
+            <br>
             <button type="submit" class="btn">Utwórz</button>
-            <button type="submit" class="btn cancel" id="form-popup-hide">Anuluj</button>
+            <button type="submit" class="btn cancel" onclick="closeAddPageForm()">Anuluj</button>
         </form>
     </div>
 </main>

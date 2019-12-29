e4a4e1420cd8e2d896ad82395e38b0fd173daf70
Brotholomew <43090838+Brotholomew@users.noreply.github.com>
2019-12-29 20:53:34 +0100

    linux / win differences vol2

--- admin/classes/Pages.php
+++ admin/classes/Pages.php
@@ -10,7 +10,7 @@ class Pages
         //if(!checkName($pageName)) return false;
         //if(!checkPath($pagePath)) return false;
         //if(!checkTemplate($pageTemplate)) return false;
-        echo ABS_PATH.'\admin\assets\editor\templates'.'<br>';
+        echo ABS_PATH.'/admin/assets/editor/templates'.'<br>';
         //echo $pageName;
         //echo "'". ABS_PATH . "/content/pages/" . $pageName . "'";
         $template = new Templates();

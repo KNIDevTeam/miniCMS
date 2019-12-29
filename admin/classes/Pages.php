8fa1fb13a5e4d31f804c982ef27c825b6ed5c6f5
Brotholomew <43090838+Brotholomew@users.noreply.github.com>
2019-12-29 20:50:00 +0100

    typos correction xd

--- admin/classes/Pages.php
+++ admin/classes/Pages.php
@@ -10,11 +10,12 @@ class Pages
         //if(!checkName($pageName)) return false;
         //if(!checkPath($pagePath)) return false;
         //if(!checkTemplate($pageTemplate)) return false;
-
-        echo $pageName;
-        echo "'". ABS_PATH . "/content/pages/" . $pageName . "'";
-
-
+        echo ABS_PATH.'\admin\assets\editor\templates'.'<br>';
+        //echo $pageName;
+        //echo "'". ABS_PATH . "/content/pages/" . $pageName . "'";
+        $template = new Templates();
+        foreach ($template->templateList as $template)
+            echo $template.'<br>';
         return true;
     }
 }

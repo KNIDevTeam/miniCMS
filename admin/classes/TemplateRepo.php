7e71380e016bb52789cb46fa349cb529bfbdc952
Wojciech Drochomirecki <witek.droch@gmail.com>
2020-02-27 12:28:04 +0100

    fixed get template bug

--- admin/classes/TemplateRepo.php
+++ admin/classes/TemplateRepo.php
@@ -24,15 +24,20 @@ class TemplateRepo implements TemplateRepoInterface
         $this->initialised = $tmpInitialised;
     }
 
-    public function getTemplate($name)
+    public function getTemplate($templname)
     {
-        return $this->templateList[$name];
+        return $this->templateList[$templname];
+    }
+
+    public function listTemplates()
+    {
+        return $this->templateList;
     }
 
     private function generate()
     {
         $startPath = $this->TemplatesPath;
-        #Its not in __construct because we may need to move initialisation away from object initialisation
+        #Its not in __construct because we may need to move initialization away from object initialization
         $this->initialised = true;
         $this->listTemplateDirectory($startPath);
         return $this->initialised;
@@ -41,7 +46,7 @@ class TemplateRepo implements TemplateRepoInterface
     private function addTemplate($name, $directory)
     {
         if(!in_array($name, array_keys($this->templateList)))
-            $this->templateList['name'] = new Template($name, $directory);
+            $this->templateList[$name] = new Template($name, $directory);
         else
             $this->initialised = false;
             #TODO maybe we want to accept same templates names and take only first one to consider

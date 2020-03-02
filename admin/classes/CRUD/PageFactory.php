af729ee77e54a284e5373d1bc6c9b688309fc184
Wojciech Drochomirecki <witek.droch@gmail.com>
2020-03-02 14:52:53 +0100

    second tmp fix of spaces to _

--- admin/classes/CRUD/PageFactory.php
+++ admin/classes/CRUD/PageFactory.php
@@ -9,6 +9,6 @@ class PageFactory implements PageFactoryInterface
     public function buildPage($pageName, $path) : PageInterface
     {
 
-        return new Page(str_replace(" ", "_", $pageName), $path); // TODO replace this with proper spaces to _ changing
+        return new Page(str_replace(" ", "%20", $pageName), $path); // TODO replace this with proper spaces to _ changing
     }
 }

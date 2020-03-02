6e2ed4fb4c74b459178e7a884039bc3297c00009
RazorMeister <battnik90@gmail.com>
2020-03-02 13:46:23 +0100

    Move <script> tags to body

--- admin/pages/views/layout.php
+++ admin/pages/views/layout.php
@@ -66,14 +66,11 @@
             <a href="#">&copy; KNI dev team</a>
         </footer>
     </div>
+
+    <!-- JQuery -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    <!-- Main script -->
+    <script src="<?php asset('assets/js/main.js') ?>"></script>
 </body>
-
-<!-- JQuery -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-
-<!-- Main script -->
-<script src="<?php asset('assets/js/main.js') ?>"></script>
-
-
-
 </html>

9535ae2c56bf8f9ee39a058c063b02624c87f8c7
Dawid Alimowski <dawid.alimowski@gmail.com>
2020-04-10 15:53:53 +0200

    Menu styling

--- content/themes/mainTheme/assets/js/main.js
+++ content/themes/mainTheme/assets/js/main.js
@@ -1,36 +1,13 @@
-/* Hide/unhide menu */
+/* Hide sub-menus */
 
-$("#menu-button").click(function() {
-    $("#menu").toggleClass("hide");
-    $("#main").toggleClass("lesspadding");
+$(".sub-menu").each(function() {
+    $(this).css("display", "none");
 });
 
-/* Add buttons to expandable menus */
+/* Activate parents of active node */
 
-$(".expand").each(function() {
-    $(this).append(`<div class="expand-button expand-button-inactive ${this.id}"><</div>`);
-});
+$(".active").parentsUntil(".main-menu").addClass("active");
 
-/* Expand submenumenu on click */
+/* Show active nodes */
 
-$(".expand").click(function() {
-    let id = this.id;
-    $(".submenu").each(function() {
-        if($(this).hasClass(id)) $(this).slideToggle();
-    });
-    $(".expand-button").each(function() {
-        if($(this).hasClass(id)) {
-            if($(this).hasClass("expand-button-inactive")) {
-                $(this).html("⌄");
-                $(this).css("top", "-0.1em")
-                $(this).removeClass("expand-button-inactive");
-                $(this).addClass("expand-button-active");
-            } else {
-                $(this).html("<");
-                $(this).css("top", "0.3em")
-                $(this).removeClass("expand-button-active");
-                $(this).addClass("expand-button-inactive");
-            }
-        }
-    });
-});
+$(".active").children().css("display", "inherit");

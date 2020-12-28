7677073d2ac86c28d132268eb8efae856906ff85
Dawid Alimowski <dawid.alimowski@gmail.com>
2020-12-28 13:43:52 +0100

    Remove deprecated submenu js

--- content/themes/default/assets/admin/js/main.js
+++ content/themes/default/assets/admin/js/main.js
@@ -1,34 +1,3 @@
-
-/* Add buttons to expandable menus */
-
-$(".expand").each(function() {
-    $(this).append(`<div class="expand-button expand-button-inactive ${this.id}"><</div>`);
-});
-
-/* Expand submenumenu on click */
-
-$(".expand").click(function() {
-    let id = this.id;
-    $(".submenu").each(function() {
-        if($(this).hasClass(id)) $(this).slideToggle();
-    });
-    $(".expand-button").each(function() {
-        if($(this).hasClass(id)) {
-            if($(this).hasClass("expand-button-inactive")) {
-                $(this).html("⌄");
-                $(this).css("top", "-0.1em")
-                $(this).removeClass("expand-button-inactive");
-                $(this).addClass("expand-button-active");
-            } else {
-                $(this).html("<");
-                $(this).css("top", "0.3em")
-                $(this).removeClass("expand-button-active");
-                $(this).addClass("expand-button-inactive");
-            }
-        }
-    });
-});
-
 /* Fix menu when scrolled down */
 
 $(window).on("scroll", function() {

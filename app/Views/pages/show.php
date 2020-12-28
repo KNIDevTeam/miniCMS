891cb9899bbcbd7ed1bd84050c152ff756bfb942
Dawid Alimowski <dawid.alimowski@gmail.com>
2020-12-28 14:41:57 +0100

    Site list experiment

--- app/Views/pages/show.php
+++ app/Views/pages/show.php
@@ -2,17 +2,18 @@
 
 <?php $this->startSection('content') ?>
 <main id="main">
+
     <div class="tiles">
         <?php
             foreach ($pages as $page) {
                 if($page == "Home") {
-                    echo "<div class=\"tile nohover\"><span class=\"tile-title\">".$page."</span>
-                    <span class=\"tile-description\"><a href='edit?name=".$page."'><i class=\"fas fa-pen\"></i>edytuj</a></span></div>";
+                    echo "<div class=\"tile thin\"><span class=\"tile-title\">".$page."</span>
+                    <span class=\"tile-description\"><a href='edit?name=".$page."'><i class=\"fas fa-pen\"></i> edytuj</a></span></div>";
                 }
                 else {
-                    echo "<div class=\"tile nohover\"><span class=\"tile-title\">".$page."</span>
-                    <span class=\"tile-description\"><a href='edit?name=".$page."'><i class=\"fas fa-pen\"></i>edytuj</a></br>".
-                    "<a href='delete?name=".$page."'><i class=\"far fa-trash-alt\"></i>usuń</a></span></div>";
+                    echo "<div class=\"tile thin\"><span class=\"tile-title\">".$page."</span>
+                    <span class=\"tile-description\"><a href='edit?name=".$page."'><i class=\"fas fa-pen\"></i> edytuj</a></br>".
+                    "<a href='delete?name=".$page."'><i class=\"far fa-trash-alt\"></i> usuń</a></span></div>";
                 }
             }
 
